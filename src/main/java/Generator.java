59bbae734e55c4548a47fb28bdb8235453cc521b
Roman Bulinski <aboandrus@gmail.com>
2019-03-31 20:56:44 +0200

    Refactor code

--- src/main/java/Generator.java
+++ src/main/java/Generator.java
@@ -4,11 +4,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
-//    Create a class that has three fields: startX, startY, length.
-//    Then write a method that accepts an array of integers and returns a list of battleships.
-//    The input array is structured in a following way:
-//            [ [ship1_startX, ship1_startY, ship1_length], [ship2_startX, ship2_startY, ship2_length], ...]
-
 public class Generator {
 
 
---
+++ src/main/source/readMe.txt
@@ -0,0 +1,4 @@
+//    Create a class that has three fields: startX, startY, length.
+//    Then write a method that accepts an array of integers and returns a list of battleships.
+//    The input array is structured in a following way:
+//            [ [ship1_startX, ship1_startY, ship1_length], [ship2_startX, ship2_startY, ship2_length], ...]
